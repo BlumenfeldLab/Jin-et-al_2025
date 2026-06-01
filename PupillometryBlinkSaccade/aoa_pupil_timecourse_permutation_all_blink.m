26d858b521d9a81f900c074ca270011486bb69a2
David Jin <47618105+davidsjin@users.noreply.github.com>
2026-05-31 21:08:02 -0400

    Update aoa_pupil_timecourse_permutation_all_blink.m

--- PupillometryBlinkSaccade/aoa_pupil_timecourse_permutation_all_blink.m
+++ PupillometryBlinkSaccade/aoa_pupil_timecourse_permutation_all_blink.m
@@ -97,10 +97,10 @@ baseline_window = 1:1000;
 %Subtract main data from subtract data
 group_pc_data = all_avg_pupil_aware - all_avg_pupil_unaware;
 
-%Calculate baseline values [voxel x time x subjects]
+%Calculate baseline values 
 group_pc_baseline = squeeze(nanmean(group_pc_data(:,baseline_window,:),2));
 
-%Convert baseline [channel x time x subjects]
+%Convert baseline 
 group_aware_minus_unaware_baseline = permute(repmat(group_pc_baseline,[1,1,4000]),[1,3,2]);
 
 %Subtract baseline from main data
@@ -111,10 +111,10 @@ aware_minus_unaware_mean_blink = nanmean(group_aware_minus_unaware_baselined_dat
 
 %aware
 
-%Calculate baseline values [channel x subjects]
+%Calculate baseline values
 group_pc_baseline = squeeze(nanmean(all_avg_pupil_aware(:,baseline_window,:),2));
 
-%Convert baseline [channel x time x subjects]
+%Convert baseline
 group_aware_baseline = permute(repmat(group_pc_baseline,[1,1,4000]),[1,3,2]);
 
 %Subtract baseline from main data
@@ -125,10 +125,10 @@ aware_mean_blink = nanmean(group_aware_baselined_data,3);
 
 %unaware
     
-%Calculate baseline values [voxel x time x subjects]
+%Calculate baseline values
 group_pc_baseline = squeeze(nanmean(all_avg_pupil_unaware(:,baseline_window,:),2));
 
-%Convert baseline [channel x time x subjects]
+%Convert baseline
 group_unaware_baseline = permute(repmat(group_pc_baseline,[1,1,4000]),[1,3,2]);
 
 %Subtract baseline from main data
@@ -141,9 +141,7 @@ unaware_mean_blink = nanmean(group_unaware_baselined_data,3);
 
 %Select channels (21 = Fz; 257 = Cz; 101 = Pz; 126 = Oz)
 eye_list = [1];
-%eye_list = 1;
-%eye_list = [183,257,101,59,155]
-%eye_name = {'C4','Cz','Pz','C3','E155'};
+
 
 %Loop over channels
 tic
@@ -153,7 +151,6 @@ for chan = 1:length(eye_list)
     
     %Define channel
     current_channel = eye_list(chan);
-    %current_eye_name = eye_name{chan};
     current_eye_name = ['E' num2str(eye_list(chan))];
     %aware vs unaware Testing
     disp(['Running Permutation Tests - ',num2str(current_channel)])
